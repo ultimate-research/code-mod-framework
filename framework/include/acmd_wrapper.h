a825cb578c96b37fe8c878b4e3a3262b6a4fe451
jugeeya <jugeeya@live.com>
2019-10-18 14:23:38 -0700

    add namespace to sv_module_access, add sv_battle_object functions

--- framework/include/acmd_wrapper.h
+++ framework/include/acmd_wrapper.h
@@ -9,6 +9,7 @@
 #include "app/sv_system.h"
 #include "app/sv_module_access.h"
 #include "app/FL_sv_module_access.h"
+#include "app/sv_battle_object.h"
 #include "lib/l2c_imports.h"
 
 #include <initializer_list>
